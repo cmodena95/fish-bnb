6c5eb04908251ca1ffcd9a531282a18c2e57ea0c
Camila Modena <itscamilla@hotmail.com>
2020-12-01 15:56:38 +0000

    Updated fish seed and added user seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_12_01_110518) do
+ActiveRecord::Schema.define(version: 2020_12_01_122717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,11 +41,11 @@ ActiveRecord::Schema.define(version: 2020_12_01_110518) do
     t.string "name"
     t.text "description"
     t.integer "price"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_fish_on_user_id"
   end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -60,4 +59,6 @@ ActiveRecord::Schema.define(version: 2020_12_01_110518) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "fish", "users"
 end
