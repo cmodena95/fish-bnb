aa38f836139a7a940de816816c318458687eacd7
Camila Modena <itscamilla@hotmail.com>
2020-12-01 13:14:23 +0000

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require "open-uri"
+
+file = URI.open('https://images.unsplash.com/photo-1601302683431-0113e9a05d09?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080')
+
+puts 'Destroying fish(es)...'
+
+Fish.destroy_all
+
+puts 'Generating new and improved fish...'
+
+10.times do
+  name = Faker::GreekPhilosophers.name
+  description = "#{Faker::Demographic.marital_status} #{Faker::Demographic.sex} #{Faker::Demographic.demonym} fish. #{Faker::Demographic.height(unit: :imperial)}"
+  location = Faker::Address.city
+  price = "#{rand(1..10)} €"
+  fish = Fish.new(name: name, description: description, location: location, price: price)
+  fish.photo.attach(io: file, filename: 'fish.png', content_type: 'image/png')
+  fish.save
+end
+
+puts 'Finished :)'
+
+
+
+
