dce70df108fbb59131069aa9417b381ad8e78415
Renan Alves <renanalvesdelima@gmail.com>
2020-12-03 14:38:33 +0100

    changed the fish show to display bookings and also touch the navbar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  def new
+    @fish = Fish.find(params[:fish_id])
+  end
+
+  def create
+    @fish = Fish.find(params[:fish_id])
+    @booking = Booking.new(booking_params)
+    @booking.fish = @fish
+    @booking.status = "pending"
+    if @booking.save
+      # redirect_to fish_path(@booking.fish) #redirect to booking index when ready
+    else
+      redirect_to fish_path(@booking.fish)
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status, :start_date, :end_date)
+  end
 end
