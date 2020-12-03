dd56800aef11b956c5dc3c2ff82a1f3e507dd257
Alvaro De la rica <alvaro.delarica.alvarez@gmail.com>
2020-12-03 17:09:38 +0100

    index update1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :fishes
-  has_many :booked_fishes, through: :bookings
+  has_many :booked_fishes, through: :bookings, source: :fishes
   has_many :bookings
+  has_many :received_bookings, through: :fishes, source: :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
